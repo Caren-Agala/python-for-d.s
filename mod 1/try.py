efe57b9bd30a36dff72694c9bb42e9ff61b72b66
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-17 11:50:40 +0300

    while false

--- mod 1/try.py
+++ mod 1/try.py
@@ -1,13 +1,14 @@
-total = 0
-i = 0
-while i < 5:
-    age = int(input('age: '))
-    i = i + 1
-    if age <= 3:
-        continue
-    elif age > 3:
-        total += 100
+# total = 0
+# i = 0
+# while i < 5:
+#     age = int(input('age: '))
+#     i = i + 1
+#     if age <= 3:
+#         continue
+#     elif age > 3:
+#         total += 100
     
-print(total)
+# print(total)
 
-    
+while False:
+  print("Looping...")
