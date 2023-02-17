7d5d7eca8b94467ce3d651b38d56a1fd5131ab1c
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-17 11:33:07 +0300

    ticket  calculator

--- mod 1/try.py
+++ mod 1/try.py
@@ -1,5 +1,13 @@
-height = float(input("Height: "))
-weight = float(input("Weight: "))
+total = 0
+i = 0
+while i < 5:
+    age = int(input('age: '))
+    i = i + 1
+    if age <= 3:
+        continue
+    elif age > 3:
+        total += 100
+    
+print(total)
 
-BMI = (weight/(height **2))
-print(BMI)
+    
