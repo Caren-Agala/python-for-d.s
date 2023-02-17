38bb6abf606f9116d37a4ae4152436a4a4123e52
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-17 12:43:03 +0300

    bmi

--- mod 1/try.py
+++ mod 1/try.py
@@ -1,14 +1,15 @@
-# total = 0
-# i = 0
-# while i < 5:
-#     age = int(input('age: '))
-#     i = i + 1
-#     if age <= 3:
-#         continue
-#     elif age > 3:
-#         total += 100
-    
-# print(total)
+weight = float(input())
+height = float(input())
 
-while False:
-  print("Looping...")
+
+bmi = weight/height**2
+
+if bmi<=18.5:
+    print("Underweight")
+elif bmi<=25:
+    print("Normal")
+elif bmi<=30:
+    print("Overweight")
+elif bmi>=30:
+    print("Obesity")
+    
